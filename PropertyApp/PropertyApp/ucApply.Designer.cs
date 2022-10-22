fca11de38c310c0593f1250d66f28d7bddefa61e
Biyela Socosa <s220490414@mandela.ac.za>
2022-10-22 21:37:38 +0200

    User Control Apply

---
+++ PropertyApp/PropertyApp/ucApply.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace PropertyApp
+{
+    partial class ucApply
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtpStartDate = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dtpEndDate = new System.Windows.Forms.DateTimePicker();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.imgProperty = new System.Windows.Forms.PictureBox();
+            this.btnApply = new System.Windows.Forms.Button();
+            this.dgvRental = new System.Windows.Forms.DataGridView();
+            this.lblPropertyID = new System.Windows.Forms.Label();
+            this.btnRent = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgProperty)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRental)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnRent);
+            this.groupBox1.Controls.Add(this.lblPropertyID);
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Controls.Add(this.btnSearch);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtSearch);
+            this.groupBox1.Controls.Add(this.imgProperty);
+            this.groupBox1.Controls.Add(this.btnApply);
+            this.groupBox1.Font = new System.Drawing.Font("Mongolian Baiti", 10.75F);
+            this.groupBox1.Location = new System.Drawing.Point(9, 16);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(751, 239);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Rental";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.dtpStartDate);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.dtpEndDate);
+            this.panel1.Location = new System.Drawing.Point(30, 104);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(443, 126);
+            this.panel1.TabIndex = 16;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Mongolian Baiti", 12.25F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(99, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(235, 18);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Enter Duration of your stay:";
+            // 
+            // dtpStartDate
+            // 
+            this.dtpStartDate.Location = new System.Drawing.Point(111, 47);
+            this.dtpStartDate.Name = "dtpStartDate";
+            this.dtpStartDate.Size = new System.Drawing.Size(269, 24);
+            this.dtpStartDate.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Start Date";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(35, 85);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "End Date";
+            // 
+            // dtpEndDate
+            // 
+            this.dtpEndDate.Location = new System.Drawing.Point(111, 85);
+            this.dtpEndDate.Name = "dtpEndDate";
+            this.dtpEndDate.Size = new System.Drawing.Size(269, 24);
+            this.dtpEndDate.TabIndex = 7;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(384, 30);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(89, 31);
+            this.btnSearch.TabIndex = 15;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 16);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Search Property ";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(158, 35);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(220, 24);
+            this.txtSearch.TabIndex = 13;
+            // 
+            // imgProperty
+            // 
+            this.imgProperty.Location = new System.Drawing.Point(497, 20);
+            this.imgProperty.Name = "imgProperty";
+            this.imgProperty.Size = new System.Drawing.Size(229, 166);
+            this.imgProperty.TabIndex = 12;
+            this.imgProperty.TabStop = false;
+            // 
+            // btnApply
+            // 
+            this.btnApply.ForeColor = System.Drawing.Color.Red;
+            this.btnApply.Location = new System.Drawing.Point(616, 195);
+            this.btnApply.Name = "btnApply";
+            this.btnApply.Size = new System.Drawing.Size(92, 36);
+            this.btnApply.TabIndex = 10;
+            this.btnApply.Text = "Apply Now";
+            this.btnApply.UseVisualStyleBackColor = true;
+            this.btnApply.Click += new System.EventHandler(this.btnApply_Click);
+            // 
+            // dgvRental
+            // 
+            this.dgvRental.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvRental.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRental.Location = new System.Drawing.Point(22, 262);
+            this.dgvRental.Name = "dgvRental";
+            this.dgvRental.Size = new System.Drawing.Size(724, 176);
+            this.dgvRental.TabIndex = 13;
+            this.dgvRental.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvRental_CellClick);
+            // 
+            // lblPropertyID
+            // 
+            this.lblPropertyID.AutoSize = true;
+            this.lblPropertyID.Location = new System.Drawing.Point(714, 206);
+            this.lblPropertyID.Name = "lblPropertyID";
+            this.lblPropertyID.Size = new System.Drawing.Size(12, 16);
+            this.lblPropertyID.TabIndex = 17;
+            this.lblPropertyID.Text = ".";
+            this.lblPropertyID.Visible = false;
+            // 
+            // btnRent
+            // 
+            this.btnRent.ForeColor = System.Drawing.Color.Red;
+            this.btnRent.Location = new System.Drawing.Point(506, 195);
+            this.btnRent.Name = "btnRent";
+            this.btnRent.Size = new System.Drawing.Size(95, 38);
+            this.btnRent.TabIndex = 18;
+            this.btnRent.Text = "Rent Now";
+            this.btnRent.UseVisualStyleBackColor = true;
+            this.btnRent.Click += new System.EventHandler(this.btnRent_Click);
+            // 
+            // ucApply
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dgvRental);
+            this.Name = "ucApply";
+            this.Size = new System.Drawing.Size(768, 453);
+            this.Load += new System.EventHandler(this.ucApply_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgProperty)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRental)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dtpStartDate;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dtpEndDate;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.PictureBox imgProperty;
+        private System.Windows.Forms.Button btnApply;
+        private System.Windows.Forms.DataGridView dgvRental;
+        private System.Windows.Forms.Label lblPropertyID;
+        private System.Windows.Forms.Button btnRent;
+    }
+}
---
+++ PropertyApp/PropertyApp/ucApply.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DAL;
+using BLL;
+using System.IO;
+
+namespace PropertyApp
+{
+    public partial class ucApply : UserControl
+    {
+        public ucApply()
+        {
+            InitializeComponent();
+        }
+        BusinessLogicLayer bll = new BusinessLogicLayer();
+        private void ucApply_Load(object sender, EventArgs e)
+        {
+            dgvRental.DataSource = bll.GetProperty();
+            dgvRental.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            dgvRental.DataSource = bll.GetPropertyByDesc(txtSearch.Text);
+            dgvRental.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        private void dgvRental_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dgvRental.SelectedRows.Count > 0)
+            {
+                lblPropertyID.Text = dgvRental.SelectedRows[0].Cells["PropertyID"].Value.ToString();
+
+                byte[] image = (byte[])dgvRental.SelectedRows[0].Cells["Image"].Value;
+                MemoryStream ms = new MemoryStream(image);
+                imgProperty.Image = Image.FromStream(ms);
+            }
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnApply_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = true;
+            btnApply.Visible = false;
+            btnRent.Visible = true;
+        }
+
+        private void btnRent_Click(object sender, EventArgs e)
+        {
+            DataTable dt = frmLogin.dtLog;
+            int tenId = int.Parse(dt.Rows[0]["UserID"].ToString());
+
+            Rental rent = new Rental(int.Parse(lblPropertyID.Text), tenId, dtpStartDate.Text, dtpEndDate.Text);
+            int x = bll.InsertRental(rent);
+            if(x > 0)
+            {
+                MessageBox.Show("Rental Successful");
+            }
+        }
+    }
+}
